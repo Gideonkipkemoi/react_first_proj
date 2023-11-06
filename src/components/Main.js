e0222bf7137daaff8cf2e04a7ad0a73162dd69f1
GideonKipkemoi <rotichgideon760@gmail.com>
2023-11-06 16:05:51 +0300

    Letter spacing in tailwing - tracking-tighter

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Main = () => (
     <div>
         <main className='px-14 py-7  text-white'>
-            <h1 className='m-0 text-4xl'>Fun facts about React</h1>
-            <ul className='list-disc marker:text-green pl-6 text-base'>
+            <h1 className='m-0 text-4xl tracking-tighter'>Fun facts about React</h1>
+            <ul className='list-disc marker:text-green marker:text-2xl pl-6 text-base mt-10 [&>*]:p-1'>
                 <li>was first released in 2013</li>
                 <li>was originally created by Jordan Walke</li>
                 <li>has well over 100k stars on Github</li>
